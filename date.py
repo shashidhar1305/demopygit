ef4dac54f8dbfc277ce2ddbef37f657b17dd8385
shashidhar1305 <61274247+shashidhar1305@users.noreply.github.com>
2021-01-07 16:15:49 +0530

    Create date.py

---
+++ date.py
@@ -0,0 +1,14 @@
+import time
+import datetime
+print("current date and time: ",datetime.datetime.now())
+print("current year: ",datetime.datetime.today().strftime("%Y"))
+print("month of year: ",datetime.datetime.today().strftime("%B"))
+print("week  number of the year: ",datetime.datetime.today().strftime("%W"))
+print("weekdayof the week: ",datetime.date.today().strftime("%w"))
+print("Day of year: ",datetime.datetime.today().strftime("%j"))
+print("Day of the month: ",datetime.datetime.today().strftime("%d"))
+print("Day of  week: ",datetime.datetime.today().strftime("%A"))
+
+
+
+
